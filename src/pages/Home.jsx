c0a6a0106a6bf19e5f5e78f79de1e92ee16098d7
Administrator <dev@xn.fo>
2026-04-18 19:50:35 +0800

    analytics add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,14 @@ import Hero from "../components/Hero.jsx";
 import Navbar from "../components/Layout/Navbar.jsx";
 import Contact from "../components/Contact.jsx";
 import Experience from "../components/Experience";
+import { Analytics } from "@vercel/analytics/next"
 
 
 
 export default function MyApp() {
     return (
         <>
+            <Analytics />
             <Navbar />
             <Hero />
             <Projects />
